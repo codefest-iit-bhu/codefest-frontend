54dafb59b95541816477476868447d8f4100800f
the-dg04 <devgup04@gmail.com>
2024-10-26 23:08:31 +0530

    minor fi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ function Navbar() {
         ></div>
 
         <div
-          className={`fixed top-0 left-0 h-full w-64 bg-gray-900 text-white z-20 transition-transform duration-300 ${
+          className={`fixed top-0 left-0 h-full w-72 bg-gray-900 text-white z-20 transition-transform duration-300 ${
             isSidebarOpen ? "translate-x-0" : "-translate-x-full"
           }`}
         >
