ed460bf6398224eb2970267cf0c2b208c36a13c0
the-dg04 <devgup04@gmail.com>
2024-10-26 18:38:52 +0530

    pixelated

---
+++ src/routes/Events.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Timeline from "../components/Timeline";
+
+export default function Events(){
+    const events = [
+        { date: '2024-10-01', title: 'Event 1', description: 'Description for event 1.' },
+        { date: '2024-10-15', title: 'Event 2', description: 'Description for event 2.' },
+        { date: '2024-10-20', title: 'Event 3', description: 'Description for event 3.' },
+        { date: '2024-10-25', title: 'Event 4', description: 'Description for event 4.' },
+    ];
+    return(
+        <>
+            <Navbar />
+            <Timeline events={events} />
+        </>
+    );
+}
--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -1,9 +1,13 @@
+import GlitchText from '../components/GlitchedCodeFest';
 import Link from '../components/Link';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen text-white p-10">
-      <img src="./glitchedCodeFest.svg" alt="" width={400} />
+      {/* <img src="./glitchedCodeFest.svg" alt="" width={400} /> */}
+      <div className="w-[400px]">
+      <GlitchText />
+      </div>
       <div className="p-3 w-[400px] flex justify-center"><Link text="Explore" href="/home"/></div>
       <div className="p-3 w-[400px] flex justify-center"><Link text="Login" href="/login"/></div>
       <div className="p-3 w-[400px] flex justify-center"><Link text="Signup" href="/signup"/></div>
