d68c7b2c810131d1f5f4d22d81b9559939d904fc
the-dg04 <devgup04@gmail.com>
2024-12-07 01:16:04 +0530

    fgbhsuiudersd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 export default function Footer(){
     return(
         <div className="flex h-[400px] w-screen bg-[#4E4040] flex-wrap overflow-y-scroll no-scrollbar">
-            <div className="flex-1 flex-shrink-0 h-[400px] min-w-[400px] flex flex-col items-center justify-center gap-4 text-white">
+            <div className="flex-1 flex-shrink-0 h-[400px] min-w-[400px] flex items-center justify-center gap-4 text-white">
                 <a href="#">WEEEEEEE</a>
+                <a href="#">WEEEEEEE</a>
+
+            </div>
+            <div className="flex-1 flex-shrink-0 h-[400px] min-w-[400px] flex flex-col items-center justify-center gap-4 text-white">
+                <a href="#"><b>WEEEEEEE</b></a>
                 <a href="#">WEEEEEEE</a>
                 <a href="#">WEEEEEEE</a>
                 <a href="#">WEEEEEEE</a>
@@ -10,15 +15,7 @@ export default function Footer(){
                 <a href="#">WEEEEEEE</a>
             </div>
             <div className="flex-1 flex-shrink-0 h-[400px] min-w-[400px] flex flex-col items-center justify-center gap-4 text-white">
-                <a href="#">WEEEEEEE</a>
-                <a href="#">WEEEEEEE</a>
-                <a href="#">WEEEEEEE</a>
-                <a href="#">WEEEEEEE</a>
-                <a href="#">WEEEEEEE</a>
-                <a href="#">WEEEEEEE</a>
-            </div>
-            <div className="flex-1 flex-shrink-0 h-[400px] min-w-[400px] flex flex-col items-center justify-center gap-4 text-white">
-                <a href="#">WEEEEEEE</a>
+                <a href="#"><b>WEEEEEEE</b></a>
                 <a href="#">WEEEEEEE</a>
                 <a href="#">WEEEEEEE</a>
                 <a href="#">WEEEEEEE</a>
@@ -26,7 +23,7 @@ export default function Footer(){
                 <a href="#">WEEEEEEE</a>
             </div>
             <div className="flex-1 flex-shrink-0 h-[400px] min-w-[400px] flex flex-col items-center justify-center gap-4 text-white">
-                <a href="#">WEEEEEEE</a>
+                <a href="#"><b>WEEEEEEE</b></a>
                 <a href="#">WEEEEEEE</a>
                 <a href="#">WEEEEEEE</a>
                 <a href="#">WEEEEEEE</a>
--- src/components/PreviousSponsors.jsx
+++ src/components/PreviousSponsors.jsx
@@ -13,7 +13,7 @@ import hackerrank from '../assets/Sponsors/hackerrank.png';
 const SponsorLogo = ({ src, alt }) => (
   <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] relative">
     <img src="/previousSponsorsBox.png" alt="" className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] absolute"/>
-    <div className="absolute w-[164px] h-[165px] md:w-[248px] md:h-[248px] top-[17px] left-[17px] overflow-hidden flex items-center justify-center">
+    <div className="absolute w-[164px] h-[165px] md:w-[248px] md:h-[248px] top-[17px] left-[17px] md:top-[25px] md:left-[25px] overflow-hidden flex items-center justify-center">
     <img src={src} alt="" className="w-[167px] h-[167px] md:w-[200px] md:h-[200px]"/>
     </div>
   </div>
