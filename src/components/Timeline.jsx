7f4d559c9ceb44e7bd9bfe1a2422f3f42e44c00e
Varun-Kolanu <kolanuvarun739@gmail.com>
2025-01-04 00:16:46 +0530

    update events

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 import EventCard from "./EventCard";
 
 const Timeline = ({ event }) => {
-  console.log(event)
   return (
     <div className="timeline-container">
       <ol className="timeline">
-        {event && new Date() < new Date(event.last_date_reg.split(",").slice(0, 2).join(",")) && (
+        {event && (
           <li key={event.name} className="timeline-item">
             <Link to={`/event/${event.name}`} className="open-link">
               <EventCard
