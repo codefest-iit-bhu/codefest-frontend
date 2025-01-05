33f33e94f1748874df46e011c75430c0790e5b26
Varun-Kolanu <kolanuvarun739@gmail.com>
2025-01-05 05:54:25 +0530

    note for event CA points

--- src/components/ca_register.jsx
+++ src/components/ca_register.jsx
@@ -185,7 +185,7 @@ const CARegistration = () => {
                       <button className="cursor-pointer py-1 px-2 bg-orange-500 text-white rounded-lg">Copy</button>
                     </CopyToClipboard>
                   </div>
-                  <p className="text-sm text-gray-700 mt-2">(Share this link with new users for signing up to get points )</p>
+                  <p className="text-sm text-gray-700 mt-2">(Share this link with new users (10 points) for signing up and make referred members participate in codefest events (2 points))</p>
                 </div>
               }
               {userRequest.status === "rejected" && (
