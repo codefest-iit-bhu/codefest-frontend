8b7b6b92118d454c02882674a9a6e37eba3383ae
Mridul Athya <146724689+mridulathya@users.noreply.github.com>
2025-01-27 18:08:06 +0530

    sponsers updated

--- src/components/OurSponsors.jsx
+++ src/components/OurSponsors.jsx
@@ -1,50 +1,41 @@
 import React from "react";
 import ixigo from "../assets/Sponsors/ixigo.jpeg";
 import darkHorse from "../assets/Sponsors/project-dark-horse.jpeg";
-import codingNinjas from "../assets/Sponsors/coding-ninjas.jpeg";
-import aws from "../assets/Sponsors/aws.png";
-import cisco from "../assets/Sponsors/cisco.png";
-import mozilla from "../assets/Sponsors/mozilla1.png";
-import topcoder from "../assets/Sponsors/top-coder.png";
-import eligible from "../assets/Sponsors/eligible.png";
-import uber from "../assets/Sponsors/uber.png";
-import zebronics from "../assets/Sponsors/zebronics.png";
-import grabon from "../assets/Sponsors/grab-on.png";
-import hackerearth from "../assets/Sponsors/hackerearth.png";
-import hackerrank from "../assets/Sponsors/hackerrank.png";
+import winzo from "../assets/Sponsors/winzo.png"
+import stockedge from "../assets/Sponsors/stockedge.png"
+import lwt from "../assets/Sponsors/learnwt.png"
 
-const SponsorLogo = ({ src, alt }) => (
+const SponsorLogo = ({ src, alt ,href }) => (
+  
   <div className="w-[150px] h-[150px] md:w-[300px] md:h-[300px] relative">
+    <a href={href || "#"}
+  {...(href ? { target: "_blank", rel: "noopener noreferrer" } : {})} >
     <img
       src="/previousSponsorsBox.svg"
       alt=""
       className="w-[150px] h-[150px] md:w-[300px] md:h-[300px] absolute z-[2]"
     />
+   
     <div className="bg-white rounded-full absolute w-[125px] h-[125px] md:w-[249px] md:h-[249px] top-[13px] left-[13px] md:top-[25px] md:left-[25px] overflow-hidden flex items-center justify-center">
+    
       <img
-        src={src}
-        alt=""
-        className="w-[100px] h-[100px] md:w-[200px] md:h-[200px]"
-      />
+          src={src}
+          alt=""
+          className="w-[100px] h-[100px] md:w-[200px] md:h-[200px]"
+        />
+
     </div>
+    </a>
   </div>
 );
 
 const PreviousSponsors = () => {
   const sponsors = [
-    { src: ixigo, alt: "IXIGO logo" },
+    { src: ixigo, alt: "IXIGO logo", href: "https://www.ixigo.com/"  },
     { src: darkHorse, alt: "DARKHORSE logo" },
-    { src: codingNinjas, alt: "CODINGNINJAS logo" },
-    { src: aws, alt: "AWS logo" },
-    { src: cisco, alt: "CISCO logo" },
-    { src: mozilla, alt: "Mozilla logo" },
-    { src: topcoder, alt: "Topcoder logo" },
-    { src: eligible, alt: "Eligible logo" },
-    { src: uber, alt: "Uber logo" },
-    { src: zebronics, alt: "Zebronics logo" },
-    { src: grabon, alt: "GrabOn logo" },
-    { src: hackerearth, alt: "Hackerearth logo" },
-    { src: hackerrank, alt: "HackerRank logo" },
+    { src: winzo , alt: "Winzo logo", href: "https://www.winzogames.com/"},
+    { src: lwt , alt: "ltw logo"},
+    { src: stockedge , alt: "stockedge logo"},
   ];
 
   return (
@@ -66,7 +57,7 @@ const PreviousSponsors = () => {
       </div>
       <div className="flex flex-wrap justify-center gap-10">
         {sponsors.map((sponsor, index) => (
-          <SponsorLogo key={index} src={sponsor.src} alt={sponsor.alt} />
+          <SponsorLogo key={index} src={sponsor.src} alt={sponsor.alt} href={sponsor.href} />
         ))}
       </div>
     </div>
