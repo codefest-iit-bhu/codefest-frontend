c6d1382eb4551faa974f787414f15cdf8962d9c5
Vivek-garg-8 <gargviv18@gmail.com>
2026-01-01 13:31:44 +0530

    updated Ca page accroding to 2026 theme

--- src/components/Benefit_CA.jsx
+++ src/components/Benefit_CA.jsx
@@ -1,6 +1,110 @@
-import React, { useEffect, useState } from "react";
-import { useUser } from "../context/context";
-import axios from "../utils/axiosInstance";
+// import React, { useEffect, useState } from "react";
+// import { useUser } from "../context/context";
+// import axios from "../utils/axiosInstance";
+// const tasks = [
+//   {
+//     start: "• A certificate of recognition, authenticated by the HoD",
+//     rest: "of the Department of Computer Science and Engineering, IIT(BHU), that will boost your resume and showcase your leadership skills.",
+//   },
+//   {
+//     start: "• Unlock a treasure chest of exciting goodies",
+//     rest: "- perks you won't find anywhere else!",
+//   },
+//   {
+//     start: "• Free access",
+//     rest: "to exclusive workshops, seminars, and events organized by CodeFest that will enhance your knowledge and skills in various domains of coding.",
+//   },
+//   {
+//     start: "• Build lasting connections and professional networks",
+//     rest: "with like-minded peers within the vibrant CodeFest community.",
+//   },
+//   {
+//     start: "• Track your impact",
+//     rest: "and see your hard work pay off with the dynamic leaderboard on CodeFest’s official website. The top-ranked Campus Ambassadors will unlock exclusive rewards and recognition.",
+//   },
+//   {
+//     start: "• A lifetime experience",
+//     rest: "of being part of a dynamic community of coders eager to learn, innovate, and create an impact.",
+//   },
+// ];
+
+// const BenefitsSection = () => {
+//   // const { user } = useUser();
+//   // const [caRequestExists, setCaRequestExists] = useState(false);
+//   // useEffect(() => {
+//   //   const fetchRequest = async () => {
+//   //     const response = await axios.get("/ca/my", {
+//   //       headers: {
+//   //         Authorization: `Bearer ${localStorage.getItem("token")}`,
+//   //       },
+//   //     });
+//   //     if (response.data && Object.keys(response.data).length > 0) setCaRequestExists(true);
+//   //   };
+
+//   //   if (localStorage.getItem("token")) fetchRequest();
+//   // }, [])
+//   return (
+//     <section className="py-10 flex flex-col items-center sm:px-3 lg:w-4/5">
+//       {/* <div className="px-10 mt-12">
+//         <p className="text-3xl md:text-5xl font-bold text-white mb-10 text-center">
+//           Why should you become a
+//           <img src="/campusAmbassdor.png" alt="Campus Ambassador" />
+//         </p>
+//       </div>
+
+//       {
+//         <div>
+//           <button className="p-4 rounded-lg transition-all duration-500 hover:bg-orange-600 bg-orange-500 text-white text-lg font-bold" onClick={() => window.location.href = "/ca-register"}>
+//             {
+//               caRequestExists ? "Your Request" : "Register Now!"
+//             }
+//           </button>
+//           <a href="/ca_leaderboard">
+//             <button className="p-4 rounded-lg transition-all duration-500 hover:bg-gray-200 bg-white border-2 border-orange-500 text-orange-500 text-lg font-bold ml-5" onClick={() => window.location.href = "/ca-register"}>
+//               CA Leaderboard
+//             </button>
+//           </a>
+//           {
+//             user?.role === "admin" &&
+//             <a href="/allCaRequests">
+//               <button className="p-4 rounded-lg bg-purple-500 hover:bg-purple-700 transition-all duration-500 text-white text-lg font-bold ml-5">Admin Panel</button>
+//             </a>
+//           }
+//           {
+//             caRequestExists &&
+//             <button className="p-4 rounded-lg transition-all duration-500 hover:bg-orange-600 bg-orange-500 text-white text-lg font-bold ml-5" onClick={() => window.location.href = "/winzo-referrals"}>
+//               Winzo Referral
+//             </button>
+//           }
+//           {
+//             (caRequestExists || user?.role === "admin") &&
+//             <button className="p-4 rounded-lg transition-all duration-500 hover:bg-green-600 bg-green-500 text-white text-lg font-bold ml-5" onClick={() => window.location.href = "/winzo_leaderboard"}>
+//               Winzo Leaderboard
+//             </button>
+//           }
+
+//         </div>
+//       } */}
+
+// <ul className="w-full list-[circle] mt-5 flex flex-col gap-8">
+//   {tasks.map((task, i) => (
+//     <div key={i}>
+//       <li className="flex flex-col w-full px-10 text-wrap font-mono text-lg md:text-2xl">
+//         <span className="font-bold text-indigo-400">{task.start}</span>
+//         {task.rest}
+//       </li>
+//       {(i == 2) && <img src="/cloud1.svg" className="w-1/2" alt="" />}
+//     </div>
+//   ))}
+// </ul> 
+//     </section>
+//   );
+// };
+
+// export default BenefitsSection;
+
+
+import React from 'react'
 const tasks = [
   {
     start: "• A certificate of recognition, authenticated by the HoD",
@@ -28,77 +132,12 @@ const tasks = [
   },
 ];
 
-const BenefitsSection = () => {
-  const { user } = useUser();
-  const [caRequestExists, setCaRequestExists] = useState(false);
-  useEffect(() => {
-    const fetchRequest = async () => {
-      const response = await axios.get("/ca/my", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      if (response.data && Object.keys(response.data).length > 0) setCaRequestExists(true);
-    };
-
-    if (localStorage.getItem("token")) fetchRequest();
-  }, [])
+const Benefit_CA = () => {
   return (
-    <section className="py-10 flex flex-col items-center sm:px-3 lg:w-4/5">
-      <div className="px-10 mt-12">
-        <p className="text-3xl md:text-5xl font-bold text-white mb-10 text-center">
-          Why should you become a
-          <img src="/campusAmbassdor.png" alt="Campus Ambassador" />
-        </p>
-      </div>
+    <div className='relative flex flex-col items-center justify-center my-10 px-5 md:px-20'>
+      <img src="src/assets/CA_images/why.png" alt="" />
+    </div>
+  )
+}
 
-      {
-        <div>
-          <button className="p-4 rounded-lg transition-all duration-500 hover:bg-orange-600 bg-orange-500 text-white text-lg font-bold" onClick={() => window.location.href = "/ca-register"}>
-            {
-              caRequestExists ? "Your Request" : "Register Now!"
-            }
-          </button>
-          <a href="/ca_leaderboard">
-            <button className="p-4 rounded-lg transition-all duration-500 hover:bg-gray-200 bg-white border-2 border-orange-500 text-orange-500 text-lg font-bold ml-5" onClick={() => window.location.href = "/ca-register"}>
-              CA Leaderboard
-            </button>
-          </a>
-          {
-            user?.role === "admin" &&
-            <a href="/allCaRequests">
-              <button className="p-4 rounded-lg bg-purple-500 hover:bg-purple-700 transition-all duration-500 text-white text-lg font-bold ml-5">Admin Panel</button>
-            </a>
-          }
-          {
-            caRequestExists &&
-            <button className="p-4 rounded-lg transition-all duration-500 hover:bg-orange-600 bg-orange-500 text-white text-lg font-bold ml-5" onClick={() => window.location.href = "/winzo-referrals"}>
-              Winzo Referral
-            </button>
-          }
-          {
-            (caRequestExists || user?.role === "admin") &&
-            <button className="p-4 rounded-lg transition-all duration-500 hover:bg-green-600 bg-green-500 text-white text-lg font-bold ml-5" onClick={() => window.location.href = "/winzo_leaderboard"}>
-              Winzo Leaderboard
-            </button>
-          }
-
-        </div>
-      }
-
-      <ul className="w-full list-[circle] mt-5 flex flex-col gap-8">
-        {tasks.map((task, i) => (
-          <div key={i}>
-            <li className="flex flex-col w-full px-10 text-wrap font-mono text-lg md:text-2xl">
-              <span className="font-bold text-indigo-400">{task.start}</span>
-              {task.rest}
-            </li>
-            {(i == 2) && <img src="/cloud1.svg" className="w-1/2" alt="" />}
-          </div>
-        ))}
-      </ul>
-    </section>
-  );
-};
-
-export default BenefitsSection;
+export default Benefit_CA
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import faqImg from "../assets/CA_images/faq.png";
+import qbgImg from "../assets/CA_images/qbg.png";
 
 const FAQ = () => {
     const [activeIndex, setActiveIndex] = useState(null);
@@ -38,25 +40,23 @@ const FAQ = () => {
     };
 
     return (
-        <div className="w-full mx-auto p-4 font-mono flex flex-col items-center">
-            <div className="flex justify-center px-40 mb-10 w-full">
-                <h1 className="text-5xl font-bold text-white z-20">FAQs</h1>
-                <img src="/cloud2.svg" alt="" className="absolute right-0 z-0 lg:block hidden" />
+        <div className="w-[80vw] flex flex-col items-center mx-auto px-4 md:px-20 font-mono min-h-[130vh]">
+            <div className="flex flex-col md:flex-row items-center justify-center px-4 md:px-10 mb-10 w-full text-center md:text-left">
+                <img src={faqImg} alt="" className="hidden md:block" />
+                <h1 className="text-2xl md:text-4xl font-bold text-[#fbeccd] [-webkit-text-stroke:2px_#726147] z-20 mx-4">Frequently Asked Questions</h1>
+                <img src={faqImg} alt="" className="hidden md:block" />
             </div>
-            <div className="space-y-4 w-[70vw] z-10 mb-4">
+            <div className="space-y-4 w-full z-10 mb-4">
                 {faqs.map((faq, index) => (
-                    <div
-                        key={index}
-                        className="border-2 border-gray-300 rounded-lg shadow-sm hover:border-purple-800"
-                    >
+                    <div key={index}>
                         <button
                             onClick={() => toggleFAQ(index)}
-                            className="w-full p-4 text-left text-lg font-medium flex justify-between items-center focus:outline-none transition-all duration-500"
+                            className="w-full p-12 px-5 md:px-14 xl:px-20 text-black text-left text-base md:text-lg font-medium flex items-center justify-between bg-cover bg-no-repeat bg-center"
+                            style={{ backgroundImage: `url(${qbgImg})` }}
                         >
-                            <span>{faq.question}</span>
+                            <span className="text-black text-sm xl:text-lg font-bold pr-4">{faq.question}</span>
                             <svg
-                                className={`w-5 h-5 transform transition-transform ${activeIndex === index ? "rotate-180" : ""
-                                    }`}
+                                className={`w-5 h-5 flex-shrink-0 transform transition-transform ${activeIndex === index ? "rotate-180" : ""}`}
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
@@ -71,7 +71,7 @@ const FAQ = () => {
                             </svg>
                         </button>
                         {activeIndex === index && (
-                            <div className="p-4 text-gray-400 bg-[#140B29] opacity-100">{faq.answer}</div>
+                            <div className="p-4 md:p-8 text-black bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#d6c999] via-[#bba87a] to-[#cab98a] opacity-100 rounded-[40px] border-solid border-[#8f774b] border-[5px]">{faq.answer}</div>
                         )}
                     </div>
                 ))}
