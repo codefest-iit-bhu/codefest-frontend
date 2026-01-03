0fb3576341f3c6960554f736fed09c338b5360cb
Hemant Ayuj <hemant.ayuj@gmail.com>
2026-01-03 20:56:25 +0530

    landing page done

--- src/backgrounds/Background.jsx
+++ src/backgrounds/Background.jsx
@@ -6,7 +6,7 @@ export default function Background({ image_path }) {
                 src={image_path}
                 width={0}
                 height={0}
-                className="absolute top-0 left-0 h-[100vh] lg:h-[110vh] xl:h-[125vh] w-fit md:w-[100vw] object-cover lg:-mt-[10vh] xl:-mt-[25vh] z-[-1] overflow-y-hidden"
+                className="absolute inset-0 w-screen h-screen object-cover object-center z-[-1]"
             />
         </>
     );
